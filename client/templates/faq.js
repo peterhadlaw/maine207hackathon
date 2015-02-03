9962a16b9190e40ce6719b9bec9c8788899f1e23
Peter Hadlaw <hadlawp@gmail.com>
2015-02-03 15:23:52 -0600

    Added FAQ

---
+++ client/templates/faq.js
@@ -0,0 +1,360 @@
+Template.faq.helpers({
+  questions: [
+    {
+      question: "Do I need any prior experience to participate?",
+      answer: Spacebars.SafeString("No formal AP Computer Science experience, or any other experience is required. <strong>This could be your first day hacking!</strong>")
+    },
+    {
+      question: "Can I just watch (and learn)?",
+      answer: "Absolutely! Come down and join us, you'll get to see what hacking is all about! Feel free to ask some of the mentors / organizers about anyting you might be interested in or have a question about."
+    },
+    {
+      question: "How much does it cost?",
+      answer: Spacebars.SafeString("<strong>0$</strong>, that's right <em>0$</em> to participate or just attend and watch! If you join within the next 5 minutes we'll even through in a free confirmation personalized with YOUR name!")
+    },
+    {
+      question: "What do I have to build / hack / create?",
+      answer: Spacebars.SafeString("Literally, <strong><em>ANYTHING</em></strong> you want to!")
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "Is this going to be fun?!?",
+      answer: "yes."
+    },
+    {
+      question: "No, but really, is this going to be fun???",
+      answer: 'Yes, but you don\'t have to take our word for it.'
+    }
+  ]
+});
