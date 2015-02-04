77631b735dc4a6573d398f64d9c130ec34299474
Peter Hadlaw <hadlawp@gmail.com>
2015-02-04 10:08:54 -0600

    Added android mobile config

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: '207Hack',
+  description: 'Official app for the Maine Township High School District 207 Hackathon',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
