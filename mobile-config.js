3db7c697b6eaf7c59c44b6dd51615c01e18adffb
Peter Hadlaw <hadlawp@gmail.com>
2015-02-04 21:51:25 -0600

    Added a bunch of assets for mobile config

--- mobile-config.js
+++ mobile-config.js
@@ -1,12 +1,48 @@
 App.info({
   name: '207Hack',
   description: 'Official app for the Maine Township High School District 207 Hackathon',
-  version: '0.0.1'
+  version: '0.0.2'
 });
 
 App.icons({
+  // iOS
+  'iphone': 'resources/icons/appicon-60.png',
+  'iphone_2x': 'resources/icons/appicon-60@2x.png',
+  'ipad': 'resources/icons/appicon-72.png',
+  'ipad_2x': 'resources/icons/appicon-72@2x.png',
+
+  // Android
   'android_ldpi': 'resources/icons/icon-ldpi.png',
   'android_mdpi': 'resources/icons/icon-mdpi.png',
   'android_hdpi': 'resources/icons/icon-hdpi.png',
   'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
+
+App.launchScreens({
+  // iOS
+  'iphone': 'resources/splash/Default.png',
+  'iphone_2x': 'resources/splash/Default@2x.png',
+  'iphone5': 'resources/splash/Default-568h.png',
+  'ipad_portrait': 'resources/splash/Default-Portrait.png',
+  'ipad_portrait_2x': 'resources/splash/Default-Portrait@2x.png',
+  'ipad_landscape': 'resources/splash/Default-Landscape.png',
+  'ipad_landscape_2x': 'resources/splash/Default-Landscape@2x.png',
+
+  // Android
+  'android_ldpi_portrait': 'resources/splash/splash-port-ldpi.png',
+  'android_ldpi_landscape': 'resources/splash/splash-land-ldpi.png',
+  'android_mdpi_portrait': 'resources/splash/splash-port-mdpi.png',
+  'android_mdpi_landscape': 'resources/splash/splash-land-mdpi.png',
+  'android_hdpi_portrait': 'resources/splash/splash-port-hdpi.png',
+  'android_hdpi_landscape': 'resources/splash/splash-land-hdpi.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-port-xhdpi.png',
+  'android_xhdpi_landscape': 'resources/splash/splash-land-xhdpi.png'
+ //'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+ //'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+ //'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+ //'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+ //'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+ //'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+ //'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+ //'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
 });
